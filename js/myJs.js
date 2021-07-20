504a17f6c7b06238c69d3ad3c7562a6b53125cb6
ducmario <86781667+ducmario@users.noreply.github.com>
2021-07-20 17:54:07 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu chị Tranggggg!",
-  text2: "Em có điều này muốn hỏi chị nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Chị yêu em có phải không nào ._.",
-  text4: "Nếu chị ko trả lời mà thoát ra tức là muốn làm vợ em rùi đó nha :v",
-  text5: "Em mơ à???",
+  text1: "He luu cậu!",
+  text2: "Tớ có điều này muốn hỏi cậu,cậu nhớ phải trả lời thật lòng nhaaa.",
+  text3: "Cậu yêu tớ có phải không nào ._.",
+  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
+  text5: "Cậu mơ à???",
   text6: "Yêu ơi là yêu <3",
-  text7: "lí do chị thích em đi :vvvv",
-  text8: "Gửi cho em <3",
-  text9: "Vì em đẹp try vlllll",
-  text10: "Em biết mà ^^ Yêu chị 300.000",
+  text7: "lí do cậu thích tớ đi :vvvv",
+  text8: "Gửi cho cậu <3",
+  text9: "Vì cậu đẹp try vlllll",
+  text10: "Cậu biết mà ^^ Yêu cậu 300.000",
   text11:
-    "Tối nay em qua đón chị đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
   text12: "Okii lunn <3",
 };
 
